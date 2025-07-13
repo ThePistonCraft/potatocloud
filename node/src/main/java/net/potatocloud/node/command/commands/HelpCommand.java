943b7e08c14253fae6fd8aa54e9d41960d729ffe
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2025-07-13 15:39:20 +0200

    help command

---
+++ node/src/main/java/net/potatocloud/node/command/commands/HelpCommand.java
@@ -0,0 +1,50 @@
+package net.potatocloud.node.command.commands;
+
+import lombok.RequiredArgsConstructor;
+import net.potatocloud.node.command.Command;
+import net.potatocloud.node.command.CommandManager;
+import net.potatocloud.node.console.Logger;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class HelpCommand implements Command {
+
+    private final Logger logger;
+    private final CommandManager commandManager;
+
+    @Override
+    public void execute(String[] args) {
+        for (Command command : commandManager.getCommands().values()) {
+            logger.info("&7" + command.getName() + getAliases(command) + " &8- " + "&7" + command.getDescription());
+        }
+    }
+
+    private String getAliases(Command command) {
+        if (command.getAliases().isEmpty()) {
+            return "";
+        }
+        return "&8[&7" + String.join(", ", command.getAliases()) + "&8]&7";
+    }
+
+
+    @Override
+    public String getName() {
+        return "help";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Shows all commands";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return List.of("?");
+    }
+
+    @Override
+    public List<String> complete(String[] args) {
+        return List.of();
+    }
+}
