bff404f52b4dfed00213bc7c4e0ac1cc1f79ba52
jirmjahu <159639233+jirmjahu@users.noreply.github.com>
2025-07-13 15:37:52 +0200

    added clear command

---
+++ node/src/main/java/net/potatocloud/node/command/commands/ClearCommand.java
@@ -0,0 +1,38 @@
+package net.potatocloud.node.command.commands;
+
+import lombok.RequiredArgsConstructor;
+import net.potatocloud.node.command.Command;
+import net.potatocloud.node.console.Console;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class ClearCommand implements Command {
+
+    private final Console console;
+
+    @Override
+    public void execute(String[] args) {
+        console.clearScreen();
+    }
+
+    @Override
+    public String getName() {
+        return "clear";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Clear console screen";
+    }
+
+    @Override
+    public List<String> getAliases() {
+        return List.of("cls");
+    }
+
+    @Override
+    public List<String> complete(String[] args) {
+        return List.of();
+    }
+}
